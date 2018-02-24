1cda02ed1156123c826cc67062ebbbca1ec8eefc
Sergii Vystoropskyi <vystos@uw.edu>
2018-02-24 15:24:19 -0800

    first step to actual java parsing

--- java2cpp/Listener.py
+++ java2cpp/Listener.py
@@ -1,5 +1,38 @@
 import Java8Listener
+from Java8Parser import Java8Parser
 
 class Listener(Java8Listener.Java8Listener):
-    def enterClassType(ctx):
-        print ctx
+    def __init__(self):
+        self._currentClass = []
+        self._classInfo = {}
+        self._methodModifiers = []
+
+    def enterNormalClassDeclaration(self, ctx):
+        CLASS_NAME_IDX = len(ctx.children) - 2
+
+        self._classInfo[ctx.children[CLASS_NAME_IDX].symbol.text] = \
+            { 'name' : ctx.children[CLASS_NAME_IDX].symbol.text}
+
+        self._currentClass.append(ctx.children[CLASS_NAME_IDX].symbol.text)
+
+    def exitNormalClassDeclaration(self, ctx):
+        self._currentClass.pop()
+
+    def enterMethodDeclaration(self, ctx):
+        modifiers = []
+        methodInfo = {}
+        for c in ctx.children:
+            if isinstance(c, Java8Parser.MethodModifierContext):
+                modifier = c.children[0].symbol.text
+
+                if modifier == u'private' or modifier == u'protected':
+                    return
+
+                modifiers.append(modifier)
+            elif isinstance(c, Java8Parser.MethodHeaderContext):
+                for cc in c.children:
+                    if isinstance(cc, Java8Parser.ResultContext):
+                        methodInfo['result'] = cc.children[0].symbol.text
+                    elif isinstance(cc, Java8Parser.MethodDeclaratorContext):
+                        methodInfo['name'] = cc.children[0].symbol.text
+        print "tmp"
--- java2cpp/main.py
+++ java2cpp/main.py
@@ -1,10 +1,34 @@
 import argparse
+import os
 
 from antlr4 import *
 from Java8Lexer import Java8Lexer
 from Java8Parser import Java8Parser
 from Listener import Listener
 
+
+def ProcessFile(srcPath, dstPath, override):
+    lexer = Java8Lexer(FileStream(srcPath))
+    stream = CommonTokenStream(lexer)
+    parser = Java8Parser(stream)
+    tree = parser.compilationUnit()
+    listener = Listener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    #TODO: generate
+
+def ProcessDirectory(srcPath, dstPath, override):
+    for path, subdirs, files in os.walk(srcPath):
+        for name in files:
+            src = os.path.join(path, name)
+            dst = os.path.join(dstPath, name)
+            ProcessFile(src, dst, override)
+
+def ProcessFiles(srcPath, dstPath, override):
+    if (os.path.isdir(srcPath)):
+        ProcessDirectory(srcPath, dstPath, override)
+    else:
+        ProcessFile(srcPath, dstPath, override)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -16,15 +40,7 @@ def main():
 
     args = parser.parse_args()
 
-    lexer = Java8Lexer(FileStream(args.input))
-    stream = CommonTokenStream(lexer)
-    parser = Java8Parser(stream)
-    tree = parser.compilationUnit()
-    listener = Listener()
-    walker = ParseTreeWalker()
-    walker.walk(listener, tree)
-
-
+    ProcessFiles(args.input, args.output, args.force)
 
 
 if __name__ == '__main__':
